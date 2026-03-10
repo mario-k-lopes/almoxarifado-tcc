8ff9ce7634a978e65826ea52bc2891a7b9952a2f
Mario.K.Lopes <kesteringmario@gmail.com>
2026-03-09 21:45:54 -0300

    Adicionada função básica para criar novos itens no almoxarifado (sim, summonar itens no almoxarifado)

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -23,8 +23,37 @@ add_form.addEventListener('submit', (event) => {
     event.preventDefault();
     console.log(add_item_name)
     const item_name = add_item_name.value.trim()
-    console.log(item_name)
+    const item_quantity = add_item_quantity.value.trim()
 
+    console.log(item_name)
+    console.log(item_quantity)
+
+    if(item_name == "" || item_quantity == "") {
+        alert("Preencha todos os campos")
+        return
+    } else if(item_quantity < 0) {
+        alert("Quantidade adicionada não pode ser menor que 0")
+        return
+    }
+
+    // Criar nova linha na minha belíssima tabela™
+    const new_line = document.createElement("tr")
+
+    //Criar novas colunas
+    const td_name = document.createElement("td")
+    const td_quantity = document.createElement("td")
+
+    // "Seta" o atributo dos itens
+    td_name.textContent = item_name
+    td_quantity.textContent = item_quantity
+
+    // Envia a maçaroca para a belíssima tabela™
+    new_line.appendChild(td_name)
+    new_line.appendChild(td_quantity)
+
+    tabela.appendChild(new_line)
+
+    // Fecha aquele pop-up feoso
     add_modal.close()
 })
 
